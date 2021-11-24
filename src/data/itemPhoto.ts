7b666943d824f42fad055a232a899dd1f7fc8e39
Rubanda <rubanda57@gmail.com>
2021-11-24 14:35:13 +0200

    added some style

---
+++ src/data/itemPhoto.ts
@@ -0,0 +1,22 @@
+interface Imgs {
+    id: number
+    pic: string
+}
+export const imgs: Imgs[] = [
+    {
+        id: 1,
+        pic: 'https://koumas-timber.com/wp-content/uploads/2020/05/Screws.jpg',
+    },
+    {
+        id: 2,
+        pic: 'https://koumas-timber.com/wp-content/uploads/2020/05/Screws.jpg',
+    },
+    {
+        id: 3,
+        pic: 'https://koumas-timber.com/wp-content/uploads/2020/05/Screws.jpg',
+    },
+    {
+        id: 4,
+        pic: 'https://koumas-timber.com/wp-content/uploads/2020/05/Screws.jpg',
+    },
+]
